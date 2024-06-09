17bbef78f9b05ee976524882c2d1d5186079c080
Dominik <zehedominik@googlemail.com>
2024-06-09 15:45:31 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dominik Zehe on 09.06.24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
